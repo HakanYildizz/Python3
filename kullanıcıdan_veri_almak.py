8d80be86180af893f9cfbbb7ec585f045f923879
Hakan Yıldız <37899913+HakanYildizz@users.noreply.github.com>
2018-04-06 23:09:22 +0300

    Create kullanıcıdan_veri_almak.py

---
+++ kullanıcıdan_veri_almak.py
@@ -0,0 +1,30 @@
+#Hakan Yıldız
+#kodları ayrı ayrı çalıştırın yoksahatadan sonrasını göremezsiniz
+
+#input() fonksiyonu
+
+ad = input("adınızı giriniz : ")
+soyad = input("soyadınızı giriniz : ")
+
+print("adınız",ad,"soyadınız",soyad,"dır.")
+
+
+#burada hata almayız mesela 2 ile 3 girdik birleştirip sonuca 23 çıktısını verir
+x = input("bir sayı giriniz : ")
+y = input("bir sayı giriniz : ")
+
+print(x+y)
+
+#sonuç doğru
+x = input("bir sayı giriniz : ")
+y = input("bir sayı giriniz : ")
+
+print(int(x)+int(y))
+
+#üstteki örnekle aynı
+x = int(input("bir sayı giriniz : "))
+y = int(input("bir sayı giriniz : "))
+
+print(x+y)
+
+
