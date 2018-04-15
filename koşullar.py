a55bfb9b94b5ac097a1981b0443c444285bb7bd5
Hakan Yıldız <37899913+HakanYildizz@users.noreply.github.com>
2018-04-15 13:09:08 +0300

    Create koşullar.py

---
+++ koşullar.py
@@ -0,0 +1,48 @@
+"""
+
+her örneği ayrı ayrı çalıştırın
+
+öncelikle aşağıdakileri bilmemiz gerekiyor
+
+==  eşittir
+
+>=  büyük eşit
+
+<=  küçük eşit
+
+!=   <>  eşit değil
+"""
+
+
+#if örneği
+
+#HATA : 5 ve 5ten küçük değer alınca herhangi brşey döndürmez
+
+x = input("bir sayı giriniz:")
+if int(x) > 5:
+    print("x 5'ten büyük.")
+
+
+#else örneği
+
+#HATA : 5 değerini girersek 5ten küçük diye döndürür
+
+x=input("bir sayı giriniz:")
+if int(x) > 5:
+    print("x 5'ten büyük.")
+else:
+    print("x 5'ten küçük.")
+    
+    
+#elif örneği
+
+#hatasız çalışır
+
+x=input("bir sayı giriniz:")
+if int(x) > 5:
+    print("x 5'ten büyük.")
+elif int(x) == 5:
+    print("x 5'e eşit.")
+else:
+    print("x 5'ten küçük.")
+    
