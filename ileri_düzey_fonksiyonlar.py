d2c59d29191b6526f997631b54ab621fb8c8e5fc
Hakan Yıldız <37899913+HakanYildizz@users.noreply.github.com>
2019-01-28 01:41:02 +0300

    Update ileri_düzey_fonksiyonlar.py

--- ileri_düzey_fonksiyonlar.py
+++ ileri_düzey_fonksiyonlar.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-https://hakanyildiz23.wordpress.com/
+https://hakannyildiz.wordpress.com/
 Hakan Yıldız
 Bu dersimizde ileri düzey fonksiyonları gördük
 Detaylı öğrenmek için yukarıdaki linke gidebilirsiniz
